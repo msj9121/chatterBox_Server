6a8ad028e3a6c354ff9ea0a659099b89bfcf91d4
Sung Jin, Moon <msj9121@gmail.com>
2019-02-12 11:56:33 +0900

    production01

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const Sequelize = require("sequelize");
 
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "production";
 const config = require("../config/config.json")[env];
 
 const sequelize = new Sequelize(
